2d2d747c00717a0cb6348dcecc7162050f012511
Squibb <rustycreepingmeat@gmail.com>
2017-02-10 13:43:35 -0500

    Committing Rest of Android Repo

---
+++ Android/BasicLocationSample/app/src/main/java/com/google/android/gms/location/sample/basiclocationsample/BootBroadcastReceiver.java
@@ -0,0 +1,75 @@
+package com.google.android.gms.location.sample.basiclocationsample;
+
+import android.app.NotificationManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
+import android.support.v4.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+/**
+ * Created by tyler on 2/8/17.
+ */
+
+// WakefulBroadcastReceiver ensures the device does not go back to sleep
+// during the startup of the service
+public class BootBroadcastReceiver extends WakefulBroadcastReceiver {
+
+    protected static final String TAG = "BootBroadcastReceiver";
+    Context cont;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        // DEBUG
+        Log.e(TAG, "BootBroadcastReceiver onReceive. #$%^ ");
+
+        // Get context
+        cont = context;
+
+        // Notification to inform user of BOOT_COMPLETED
+        showNotification();
+
+        // Launch the specified service when this message is received
+        Intent startServiceIntent = new Intent(context, com.google.android.gms.location.sample.basiclocationsample.BootService.class);
+        startWakefulService(context, startServiceIntent);
+    }
+
+    public void showNotification() {
+        // getContext
+        Context ctx = cont;
+
+        Intent intent = new Intent(ctx, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder b = new NotificationCompat.Builder(ctx);
+
+        b.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setTicker("Hearty365")
+                .setContentTitle("Location Monitoring")
+                .setContentText("Location Monitoring is now Active.")
+                .setDefaults(Notification.DEFAULT_LIGHTS| Notification.DEFAULT_SOUND)
+                .setContentIntent(contentIntent)
+                .setContentInfo("Info");
+
+
+        NotificationManager notificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, b.build());
+    }
+}
---
+++ Android/BasicLocationSample/app/src/main/java/com/google/android/gms/location/sample/basiclocationsample/BootService.java
@@ -0,0 +1,68 @@
+package com.google.android.gms.location.sample.basiclocationsample;
+
+// Imports
+import android.app.IntentService;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
+
+/**
+ * Created by tyler on 2/8/17.
+ */
+
+// Service for Networking and Location Updates
+public class BootService extends IntentService {
+
+    // Get TAG
+    final static String TAG = "BootService";
+
+    // Must create a default constructor
+    public BootService() {
+        // Used to name the worker thread, important only for debugging.
+        super("boot-service");
+
+        // DEBUG
+        Log.e(TAG, "BootService Constructor.");
+    }
+
+    // Constructor
+    @Override
+    public void onCreate() {
+        super.onCreate(); // if you override onCreate(), make sure to call super().
+        // If a Context object is needed, call getApplicationContext() here.
+
+        // DEBUG
+        Log.e(TAG, "BootService onCreate.");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        // This describes what will happen when service is triggered
+        WakefulBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    @Override
+    public ComponentName startService(Intent service) {
+        return super.startService(service);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+}
