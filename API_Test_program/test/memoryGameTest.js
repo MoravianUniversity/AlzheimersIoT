f1c038908b49e084d47173ede13669a850d2ef94
Alec Gerhart <ajgphilsfan22@gmail.com>
2017-03-17 12:30:44 -0400

    Adding more tests for posting to API using the memoryGame endpoint

--- API_Test_program/test/memoryGameTest.js
+++ API_Test_program/test/memoryGameTest.js
@@ -28,7 +28,7 @@ describe('Test the memoryGame Endpoint for data post and ensuring corrupt data d
         });
      });
 
-    it("should fail to post to memoryGame due to user being wrong data type", function(done){
+    it("should fail to post to memoryGame due to user being a number rather than a String value", function(done){
             var date = new Date();
             var postData = {"user": 1,"score": 1, "time": date.toISOString()}
             var options = {
@@ -49,7 +49,7 @@ describe('Test the memoryGame Endpoint for data post and ensuring corrupt data d
             });
     });
 
-    it("should fail to post to memoryGame due to score being wrong data type", function(done){
+    it("should fail to post to memoryGame due to score being a String rather than a number value", function(done){
             var date = new Date();
             var postData = {"user": 'User_Test',"score": "corrupted data", "time": date.toISOString()}
             var options = {
@@ -70,7 +70,7 @@ describe('Test the memoryGame Endpoint for data post and ensuring corrupt data d
             });
     });
 
-    it("should fail to post to memoryGame due to date being wrong data type", function(done){
+    it("should fail to post to memoryGame due to date being an incorrectly formatted String", function(done){
                 var date = new Date();
                 var postData = {"user": 'User_Test',"score": 5, "time": "corrupted data"}
                 var options = {
@@ -90,4 +90,173 @@ describe('Test the memoryGame Endpoint for data post and ensuring corrupt data d
                     return done();
                 });
         });
+
+    it("should fail to post to memoryGame due to time having a number type instead of a time", function(done){
+        var date = new Date();
+        var postData = {"user": "USER_TEST","score":1, "time": 1}
+        var options = {
+            method: 'POST',
+            body: postData,
+            json: true,
+            url: hostname+'/memoryGame'
+        }
+        request(options, function(err, response, body){
+            if(err){
+                console.log(err)
+                return
+            }
+            expect(response).to.exist;
+            expect(response.statusCode).to.equal(400);
+            assert(response.body.error, 'There are missing or invalid parameters in the request.');
+            return done();
+        });
+    });
+
+    it("should fail to post to memoryGame due to missing username", function(done){
+        var date = new Date();
+        var postData = {"score": 5, "time": date.toISOString()}
+        var options = {
+            method: 'POST',
+            body: postData,
+            json: true,
+            url: hostname+'/memoryGame'
+        }
+        request(options, function(err, response, body){
+            if(err){
+                console.log(err)
+                return
+            }
+            expect(response).to.exist;
+            expect(response.statusCode).to.equal(400);
+            assert(response.body.error, 'There are missing or invalid parameters in the request.');
+            return done();
+        });
+    });
+
+    it("should fail to post to memoryGame due to missing score value", function(done){
+        var date = new Date();
+        var postData = {"user": "User_Test", "time": date.toISOString()}
+        var options = {
+            method: 'POST',
+            body: postData,
+            json: true,
+            url: hostname+'/memoryGame'
+        }
+        request(options, function(err, response, body){
+            if(err){
+                console.log(err)
+                return
+            }
+            expect(response).to.exist;
+            expect(response.statusCode).to.equal(400);
+            assert(response.body.error, 'There are missing or invalid parameters in the request.');
+            return done();
+        });
+    });
+
+    it("should fail to post to memoryGame due to missing time value", function(done){
+        var date = new Date();
+        var postData = {"user": "User_Test", "score": 1}
+        var options = {
+            method: 'POST',
+            body: postData,
+            json: true,
+            url: hostname+'/memoryGame'
+        }
+        request(options, function(err, response, body){
+            if(err){
+                console.log(err)
+                return
+            }
+            expect(response).to.exist;
+            expect(response.statusCode).to.equal(400);
+            assert(response.body.error, 'There are missing or invalid parameters in the request.');
+            return done();
+        });
+    });
+
+    it("should fail to post to memoryGame due to missing both user and score values", function(done){
+        var date = new Date();
+        var postData = {"time": date.toISOString()}
+        var options = {
+            method: 'POST',
+            body: postData,
+            json: true,
+            url: hostname+'/memoryGame'
+        }
+        request(options, function(err, response, body){
+            if(err){
+                console.log(err)
+                return
+            }
+            expect(response).to.exist;
+            expect(response.statusCode).to.equal(400);
+            assert(response.body.error, 'There are missing or invalid parameters in the request.');
+            return done();
+        });
+    });
+
+    it("should fail to post to memoryGame due to missing user and time values", function(done){
+        var date = new Date();
+        var postData = {"score": 1}
+        var options = {
+            method: 'POST',
+            body: postData,
+            json: true,
+            url: hostname+'/memoryGame'
+        }
+        request(options, function(err, response, body){
+            if(err){
+                console.log(err)
+                return
+            }
+            expect(response).to.exist;
+            expect(response.statusCode).to.equal(400);
+            assert(response.body.error, 'There are missing or invalid parameters in the request.');
+            return done();
+        });
+    });
+
+    it("should fail to post to memoryGame due to missing score and time values", function(done){
+        var date = new Date();
+        var postData = {"user": "User_Test"}
+        var options = {
+            method: 'POST',
+            body: postData,
+            json: true,
+            url: hostname+'/memoryGame'
+        }
+        request(options, function(err, response, body){
+            if(err){
+                console.log(err)
+                return
+            }
+            expect(response).to.exist;
+            expect(response.statusCode).to.equal(400);
+            assert(response.body.error, 'There are missing or invalid parameters in the request.');
+            return done();
+        });
+    });
+
+    it("should fail to post to memoryGame due to missing all values", function(done){
+        var date = new Date();
+        var postData = {}
+        var options = {
+            method: 'POST',
+            body: postData,
+            json: true,
+            url: hostname+'/memoryGame'
+        }
+        request(options, function(err, response, body){
+            if(err){
+                console.log(err)
+                return
+            }
+            expect(response).to.exist;
+            expect(response.statusCode).to.equal(400);
+            assert(response.body.error, 'There are missing or invalid parameters in the request.');
+            return done();
+        });
+    });
+
 })
