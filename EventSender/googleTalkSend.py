ded8c5bd55a7d28b8236eaa08896604415dd938f
collins <cowilli4@USBBCOCOWILLI4.local>
2017-03-28 16:50:30 -0400

    python sender now takes in url

--- EventSender/googleTalkSend.py
+++ EventSender/googleTalkSend.py
@@ -1,12 +1,12 @@
 import socket
 import sys
 
-messArr = sys.argv[1:]
+messArr = sys.argv[2:]
 message = ""
 for i in messArr:
     message = message + i + " "
 
-UDP_IP = socket.gethostbyname(socket.gethostname())
+UDP_IP = sys.argv[1]
 UDP_PORT = 8123
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
