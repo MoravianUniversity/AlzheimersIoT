aa1f01d4a59a1590fac5cbe8a37cbf51affe4694
Anna Lamoureux <anna.lamoureux@gmail.com>
2017-03-28 17:05:16 -0400

    Adds a file with a function that calls googleTalkSend.py with necessary arguments

---
+++ EventSender/sendMessage.py
@@ -0,0 +1,14 @@
+import os
+
+def sendStringToHome(message):
+    baseURL = "http://204.186.197.73 "
+    scriptName = "googleTalkSend.py "
+    scriptCallType = "python3 "
+
+    print(scriptCallType + scriptName + baseURL + message)
+    os.system(scriptCallType + scriptName + baseURL + message)
+
+
+if __name__ == '__main__':
+    message = "Hello Google"
+    sendStringToHome(message)
