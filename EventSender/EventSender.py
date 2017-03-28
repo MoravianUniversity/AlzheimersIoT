d4cddf8ca4d314547fc7135fc48bff03620dc86b
Charlie <stcjm08@moravian.edu>
2017-03-28 16:31:11 -0400

    EventSender updated with basic structure of operations

--- EventSender/EventSender.py
+++ EventSender/EventSender.py
@@ -5,21 +5,43 @@ class EventSender():
     def __init__(self):
         self.email = "emailAddress"
         self.phoneNumber = 8562342250
+        self.url = 'http://pegasus.cs.moravian.edu:8080/api/'
+        self.endpointNames = = ['gps', 'wemo', 'journal', 'medicineLogger', 'memoryGame']
 
     def loopForever(self):
+    	data = []
         while(True):
-            #call MMSE endpoint
-            if(self.hasEndpointChanged()):
-                self.sendNotifications()
-            time.sleep(5)
+        	for name in endpointNames:
+            	#call MMSE endpoint
+            	#get data dump
+            	data = #dump
+            	writeDataToFile(data)
+            	if(self.hasEndpointChanged()):
+                	self.sendNotifications()
+            	time.sleep(5)
 
-    def hasEndpointChanged(self):
+    def getAndWriteEndpointData(name):
         #Bill's code goes here
+        data = get(self.url+name).content.decode()
+        writeDataToFile(data)
+        
+    def readAndCompareDataFile(filename):
+		with open(filename, 'r') as f:
+			for line in f:
+				#check to see if it has changed
+        
 
     def sendNotifications(self):
         #send text message
         #send email
         #make google talk speak
+        
+	def writeDataToFile(data):
+    	for name in endpointNames:
+    		if(name+'.txt' != nil)
+    			file = open(name+'.txt', "w")
+				file.write(data)
+				file.close()
 
 
 if __name__ == "__main__":
