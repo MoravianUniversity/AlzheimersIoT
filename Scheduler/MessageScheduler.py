5acd8871073d4d3f4c098a8dc9c32774a25e1b72
Josh Russett <stjkr02@moravian.edu>
2017-03-29 00:52:44 -0400

    MessageScheduler Fixed
    Fixed an issue with the timing of events, specifically, the docker image
    runs on UTC time so I had to make the time string include the UTC
    offset. (e.g. -0400 for EDT)
    API is basically complete, just need to add a linting function for the
    time param, and return an error as necessary.

--- Scheduler/MessageScheduler.py
+++ Scheduler/MessageScheduler.py
@@ -1,4 +1,5 @@
-import time, sys, datetime
+import os, requests, datetime, pytz
+
 from apscheduler.schedulers.background import BackgroundScheduler
 
 class SingletonMetaClass(type):
@@ -26,36 +27,48 @@ class MessageScheduler(metaclass=SingletonMetaClass):
     def add_message(self, args):
         medium = args['medium']
 
-        if medium is 'SMS':
+        if medium == 'SMS':
             self.__add_SMS_notification(args)
 
-        elif medium is 'Email':
+        elif medium == 'Email':
             self.__add_Email_notification(args)
 
-        elif medium is 'Google Home':
+        elif medium == 'Google Home':
             self.__add_Google_Home_notification(args)
 
 
     # SMS Methods
     def __add_SMS_notification(self, args):
-        self.__s.add_job(self.__send_SMS, 'date', run_date=time, kwargs=self.__get_kwargs_args(args))
+        self.__s.add_job(self.__send_SMS, 'date', run_date=self.__get_datetime_conversion(args['time']), kwargs=self.__get_kwargs_args(args))
 
     def __send_SMS(self, dest='UNDEFINED', msg='UNDEFINED'):
-        print("{} MessageScheduler: {}, {}".format(time.time(), dest, msg), file=sys.stderr)
+        payload = {}
+        requests.post(os.environ.get('EMAIL_API_URL'), data=payload)
+
 
     # Email Methods
     def __add_Email_notification(self, args):
-        return
+        self.__s.add_job(self.__send_Email, 'date', run_date=args['time'], kwargs=self.__get_kwargs_args(args))
+
+    def __send_Email(self, dest='UNDEFINED', msg='UNDEFINED'):
+        payload = {}
+        requests.post(os.environ.get('EMAIL_API_URL'), data=payload)
+
 
     # Google Home Methods
     def __add_Google_Home_notification(self, args):
-        return
+        self.__s.add_job(self.__send_Google_Home_TTS, 'date', run_date=args['time'], kwargs=self.__get_kwargs_args(args))
+
+    def __send_Google_Home_TTS(self, dest='UNDEFINED', msg='UNDEFINED'):
+        payload = {}
+        requests.post(os.environ.get('GOOGLE_HOME_API_URL'), data=payload)
+
 
     # General Helper Methods
-    def __calc_delay(self, given_time):
-        return given_time - time.time()
-
     def __get_kwargs_args(self, given_args):
         return {'dest': given_args['dest'], 'msg': given_args['msg']}
 
+    def __get_datetime_conversion(self, time_str):
+        d = datetime.datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S %z")
+        return d.astimezone(pytz.utc)
 
