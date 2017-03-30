d39cdf77154901fd3e96abeb3c9fb4970b4cd6ef
13howardD <danhoward1994@gmail.com>
2017-03-30 14:45:58 -0400

    SMS app updated to use new Twilio account

--- SMSSender/main.py
+++ SMSSender/main.py
@@ -4,19 +4,19 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # Twilio Credentials
-ACCOUNT_SID = "AC9edd68eff12cd59f007b719007effc31"
-AUTH_TOKEN = "eea3ef3bf4d3cd9da5e00dcb7a9ca1f2"
+ACCOUNT_SID = "AC864dbb66d4cedade1f9c1331475c7d96"
+AUTH_TOKEN = "97554434c12f7a45c4151763ee99f27b"
 
 # Create REST Client
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 
-# Do not change "from_" field. This is the Twilio account number
+# Do not change "from_" field. This is the Twilio account number (484) 895-1386
 @app.route("/sms", methods=['POST'])
 def sms():
     client.messages.create(
         to=request.form["recipient"],
-        from_="+14845440113",
+        from_="+14848951386",
         body=request.form["message"],
         # media_url="https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
     )
