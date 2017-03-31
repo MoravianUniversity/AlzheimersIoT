2a28a8cb2c0e4f7304159dd9b55d7115d00d07ec
collins <cowilli4@USBBCOCOWILLI4.local>
2017-03-31 18:31:21 -0400

    commit before merge

--- EventSender/NotificationRecipientContactInfo.py
+++ EventSender/NotificationRecipientContactInfo.py
@@ -1,2 +1,2 @@
 phoneNumbers = ["+14848929934"]
-emailAddresses = ["stabr02@moravian.edu", "colemanb@moravian.edu", "stwjc04@moravian.edu", "stmpt03@moravian.edu"]
+emailAddresses = ["stabr02@moravian.edu", "stwjc04@moravian.edu"]
