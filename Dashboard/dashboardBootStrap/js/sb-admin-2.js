13fa1d48c5e70d8194c260ca7a2af5c3481e42f2
Squibb <rustycreepingmeat@gmail.com>
2017-04-05 17:20:44 -0400

    Changes to Dashboard and GPS App
    Dashboard now grabs data for:
     - WeMo, GPS, Journal, and MemoryTest
    GPS application explicitly enables GPS and Network acquisition for
    location service, comment and code cleanup

--- Dashboard/dashboardBootStrap/js/sb-admin-2.js
+++ Dashboard/dashboardBootStrap/js/sb-admin-2.js
@@ -44,7 +44,7 @@ $(function() {
     
     var req = new XMLHttpRequest();
 
-    req.open('GET', 'http://8887eddd.ngrok.io/api/gps', true);
+    req.open('GET', 'http://accd1036.ngrok.io/api/gps', true);
     req.onreadystatechange = function() {
         if (req.readyState === 4) {
             console.log(req.responseText);
@@ -57,7 +57,7 @@ $(function() {
     
 // GPS
 $.ajax({
-        url: "http://8887eddd.ngrok.io/api/gps",
+        url: "http://accd1036.ngrok.io/api/gps",
         dataType: 'json',
         data: data
     })
@@ -79,15 +79,21 @@ $.ajax({
     
 // MMSE
 $.ajax({
-        url: "http://8887eddd.ngrok.io/api/memoryGame",
+        url: "http://accd1036.ngrok.io/api/memoryGame",
         dataType: 'json',
         data: data
     })
     .done(function(data) {
         console.log("Acquired MMSE");
         console.log(data[0]);
-        latestMG = data[0];
-        
+        var numEntries = data.length;
+        var score = data[0].score;
+        var user = data[0].user;
+        var timeD = data[0].time;
+        $(".memScore").html(score);
+        $(".memUser").html(user);
+        $(".memTime").html(timeD);
+        $(".memEntries").html(numEntries);
     })
     .fail(function(data) {
         console.log("Failed MMSE Retrieval");
@@ -95,18 +101,48 @@ $.ajax({
     
 // Journal
 $.ajax({
-        url: "http://8887eddd.ngrok.io/api/journal",
+        url: "http://accd1036.ngrok.io/api/journal",
         dataType: 'json',
         data: data
     })
     .done(function(data) {
         console.log("Acquired Journal");
         console.log(data[0]);
-        latestMG = data[0];
-        
+        var numEntries = data.length;
+        var medicationTaken = data[0].medication;
+        var message = data[0].message;
+        var timeD = data[0].datetime;
+        var activities = data[0].activities;
+        $(".jMed").html(medicationTaken);
+        $(".jMessage").html(message);
+        $(".jTime").html(timeD);
+        $(".jActivities").html(activities);
+        $(".jEntries").html(numEntries);
     })
     .fail(function(data) {
         console.log("Failed Journal Retrieval");
+    });
+
+// WeMo
+$.ajax({
+        url: "http://accd1036.ngrok.io/api/wemo",
+        dataType: 'json',
+        data: data
+    })
+    .done(function(data) {
+        console.log("Acquired WeMo");
+        console.log(data[0]);
+        var numEntries = data.length;
+        var status = data[0].status;
+        var time = data[0].time;
+        var date = data[0].date;
+        $(".wemoStatus").html(status);
+        $(".wemoTime").html(time);
+        $(".wemoDate").html(date);
+        $(".wemoEntries").html(numEntries);
+    })
+    .fail(function(data) {
+        console.log("Failed WeMo Retrieval");
     });
 
 });
