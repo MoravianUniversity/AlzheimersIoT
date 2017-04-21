e4423e92723191b0f4befecaa7ca252cd294882b
Squibb <rustycreepingmeat@gmail.com>
2017-04-20 21:39:36 -0400

    Working Dashboard
    Grabs last location from API

--- Dashboard/dashboardBootStrap/dist/js/api.js
+++ Dashboard/dashboardBootStrap/dist/js/api.js
@@ -143,5 +143,86 @@ $.ajax({
     .fail(function(data) {
         console.log("Failed WeMo Retrieval");
     });
-    
 });
+
+var watchId = null;
+function geoloc() {
+    if (navigator.geolocation) {
+        var optn = {
+                enableHighAccuracy : true,
+                timeout : Infinity,
+                maximumAge : 0
+        };
+    watchId = navigator.geolocation.watchPosition(showPosition, showError, optn);
+    } 
+    else {
+            alert('Geolocation is not supported in your browser');
+    }
+}
+
+function showPosition(position) {
+    var googlePos = new google.maps.LatLng(lati, long);
+    var mapOptions = {
+        zoom : 15,
+        center : googlePos,
+        mapTypeId : google.maps.MapTypeId.ROADMAP
+    };
+    var mapObj = document.getElementById('map');
+    var googleMap = new google.maps.Map(mapObj, mapOptions);
+    var markerOpt = {
+        map : googleMap,
+        position : googlePos,
+        title : 'Hi , I am here',
+        animation : google.maps.Animation.DROP
+    };
+    var googleMarker = new google.maps.Marker(markerOpt);
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+        'latLng' : googlePos
+    }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            if (results[1]) {
+                var popOpts = {
+                    content : results[1].formatted_address,
+                    position : googlePos
+                };
+                $("#location").text(results[1].formatted_address);
+                var popup = new google.maps.InfoWindow(popOpts);
+                google.maps.event.addListener(googleMarker, 'click', function() {
+                    popup.open(googleMap);
+                });
+            } 
+            else {
+                alert('No results found');
+            }
+        } 
+        else {
+            alert('Geocoder failed due to: ' + status);
+        }
+    });
+}
+
+function stopWatch() {
+    if (watchId) {
+        navigator.geolocation.clearWatch(watchId);
+        watchId = null;
+    }
+}
+
+function showError(error) {
+    var err = document.getElementById('map');
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+        err.innerHTML = "User denied the request for Geolocation."
+        break;
+        case error.POSITION_UNAVAILABLE:
+        err.innerHTML = "Location information is unavailable."
+        break;
+        case error.TIMEOUT:
+        err.innerHTML = "The request to get user location timed out."
+        break;
+        case error.UNKNOWN_ERROR:
+        err.innerHTML = "An unknown error occurred."
+        break;
+    }
+}
