9e508cca070eb97269d243cb017b52f36568c55a
Squibb <rustycreepingmeat@gmail.com>
2017-04-20 21:28:54 -0400

    Dashboard: Working Map Example

--- Dashboard/dashboardBootStrap/dist/js/api.js
+++ Dashboard/dashboardBootStrap/dist/js/api.js
@@ -143,51 +143,5 @@ $.ajax({
     .fail(function(data) {
         console.log("Failed WeMo Retrieval");
     });
-
-});
-
-$(document).ready(function(){
     
-    var x = document.getElementById("mapholder");
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition, showError);
-        } else { 
-            x.innerHTML = "Geolocation is not supported by this browser.";
-        }
-    }
-
-    function showPosition(position) {
-        latlon = new google.maps.LatLng(lati, long);
-        mapholder = document.getElementById("mapholder");
-        //mapholder.style.height = '250px';
-        //mapholder.style.width = '500px';
-
-        var myOptions = {
-        center:latlon,zoom:15,
-        mapTypeId:google.maps.MapTypeId.ROADMAP,
-        mapTypeControl:false,
-        navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
-        };
-
-        var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
-        var marker = new google.maps.Marker({position:latlon,map:map,title:"Last location update was here!"});
-    }
-
-    function showError(error) {
-        switch(error.code) {
-            case error.PERMISSION_DENIED:
-                x.innerHTML = "User denied the request for Geolocation.";
-                break;
-            case error.POSITION_UNAVAILABLE:
-                x.innerHTML = "Location information is unavailable.";
-                break;
-            case error.TIMEOUT:
-                x.innerHTML = "The request to get user location timed out.";
-                break;
-            case error.UNKNOWN_ERROR:
-                x.innerHTML = "An unknown error occurred.";
-                break;
-        }
-    }
 });
