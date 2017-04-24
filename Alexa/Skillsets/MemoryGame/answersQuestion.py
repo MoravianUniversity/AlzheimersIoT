32f3abfe92056aee4920941c58f4684cc553755e
Michael5011st <stmpt03@moravian.edu>
2017-04-24 14:11:29 -0400

    adds Updated MemoryGame Code

--- Alexa/Skillsets/MemoryGame/answersQuestion.py
+++ Alexa/Skillsets/MemoryGame/answersQuestion.py
@@ -1,5 +1,5 @@
 questionList = ['What is the year?',
-                      'What is the season',
+                      'What is the month',
                       'Please remember the word airplane. Please respond with okay',
                       'Repeat the phrase: No ifs ands or buts',
                       'Earlier I asked you to remember a noun. Can you please say that noun for me?']
