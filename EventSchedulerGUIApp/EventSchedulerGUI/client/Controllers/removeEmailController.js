39b39294208ae6f0b92b2f95f4371af1fb916e0c
Michael5011st <stmpt03@moravian.edu>
2017-04-27 10:35:59 -0400

    adds Removal feature to GUI
    this will add the new removal options to the GUI1

---
+++ EventSchedulerGUIApp/EventSchedulerGUI/client/Controllers/removeEmailController.js
@@ -0,0 +1,19 @@
+Template.removeEmailTemplate.events({
+    "submit #removeEmail": function (event){
+        event.preventDefault();
+        var email ={
+            emailAddress: event.target.Email.value,
+        };
+        Meteor.call('removeEmailfromDatabase',email, function(err, respJson){
+            if(err){
+                window.alert("Error: "+err.reason);
+                console.log("error occured on sending data to server. ",err);
+            }
+            else{
+                console.log("Response: ", response);
+                window.alert("successfully deleted your email");
+            }
+        })
+
+    }
+})
---
+++ EventSchedulerGUIApp/EventSchedulerGUI/client/Controllers/removePhoneNumberController.js
@@ -0,0 +1,19 @@
+Template.removePhoneNumberTemplate.events({
+    "submit #removePhoneNumber": function (event){
+        event.preventDefault();
+        var Data ={
+            PhoneNumber: event.target.PhoneNumber.value,
+        };
+        Meteor.call('removePhoneNumberfromDatabase',Data, function(err, respJson){
+            if(err){
+                window.alert("Error: "+err.reason);
+                console.log("error occured on sending data to server. ",err);
+            }
+            else{
+                console.log("Response: ", response);
+                window.alert("Successfully deleted your phone number");
+            }
+        })
+
+    }
+})
